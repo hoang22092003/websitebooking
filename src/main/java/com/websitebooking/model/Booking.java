e8a510a8b8f5adb7161d0e88f2e0f7764f53607e
hoang22092003 <luhuyhoang075@gmail.com>
2024-11-30 22:24:55 +0700

    updatecode16

---
+++ src/main/java/com/websitebooking/model/Booking.java
@@ -0,0 +1,9 @@
+package com.websitebooking.model;
+
+public class BookingRequest {
+    private Long userId;
+    private Long itemId;
+    private String itemType;
+
+    // Constructors, Getters, and Setters
+}
---
+++ src/main/java/com/websitebooking/repository/BookingRepository.java
@@ -0,0 +1,2 @@
+package com.websitebooking.repository;public interface BookingRepository {
+}
